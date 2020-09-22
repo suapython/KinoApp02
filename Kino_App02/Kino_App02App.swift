ce3d55bca8eb04d3271924cdc95027e58b0856c2
suapython <morales@roma2.infn.it>
2020-09-22 21:44:06 +0200

    Initial Commit

---
+++ Kino_App02/Kino_App02App.swift
@@ -0,0 +1,17 @@
+//
+//  Kino_App02App.swift
+//  Kino_App02
+//
+//  Created by jose francisco morales on 22/09/2020.
+//
+
+import SwiftUI
+
+@main
+struct Kino_App02App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
